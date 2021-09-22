fb7872e5274de51d14426868044acf63fe66f7bc
Luciano Dias <idh45043@gmail.com>
2021-09-22 02:47:15 +0100

    [Studio][Core] Fix installation macos

--- src/hooks/useProjects.tsx
+++ src/hooks/useProjects.tsx
@@ -1,7 +1,10 @@
 import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+import { promises as fsPromises } from 'fs';
 import * as fs from 'fs';
+
 import * as cmd from 'child_process';
 import os from 'os'
+import path from 'path'
 import extract from 'extract-zip';
 
 import { api } from "../services/api";
@@ -81,41 +84,41 @@ export function ProjectsProvider({ children }: ProjectsProviderProps) {
 
   useEffect(() => {
     api.get('project')
-      .then(response => {console.log(response.data);setProjects(response.data)})
+      .then(response => { console.log(response.data); setProjects(response.data) })
   }, [])
 
   async function createProject(projectInput: Project) {
     try {
       console.log(projectInput);
-      const responseEnvironment = await api.post('environment', { 
+      const responseEnvironment = await api.post('environment', {
         Name: `DEV-${projectInput.alias}`,
-	      DataSource: projectInput.instance,
-	      SQLUser: projectInput.sqlUser,
-	      SQLPassword: projectInput.sqlPassword,
+        DataSource: projectInput.instance,
+        SQLUser: projectInput.sqlUser,
+        SQLPassword: projectInput.sqlPassword,
         DatabasePrefix: projectInput.alias
       });
 
-      const environment : Environment = responseEnvironment.data;
+      const environment: Environment = responseEnvironment.data;
 
-      const response = await api.post('project', { 
+      const response = await api.post('project', {
         Name: projectInput.name,
-	      Description: projectInput.description,
-	      Alias: projectInput.alias,
-	      Author: projectInput.author,
-	      Customer: projectInput.customer,
-	      StartProjectDate: new Date(),
-	      BaselineProjectDate: new Date(),
-	      TemplateID: projectInput.template,
-	      EnvironmentID: environment.id,
-	      VersionControlID: null,
-	      VersionSettingID: projectInput.version
+        Description: projectInput.description,
+        Alias: projectInput.alias,
+        Author: projectInput.author,
+        Customer: projectInput.customer,
+        StartProjectDate: new Date(),
+        BaselineProjectDate: new Date(),
+        TemplateID: projectInput.template,
+        EnvironmentID: environment.id,
+        VersionControlID: null,
+        VersionSettingID: projectInput.version
       });
 
       const project = response.data;
       setProjects([...projects, project]);
-      enqueueSnackbar('Project created successfully!', { variant:'success', anchorOrigin:{vertical: 'bottom', horizontal: 'right',} });
+      enqueueSnackbar('Project created successfully!', { variant: 'success', anchorOrigin: { vertical: 'bottom', horizontal: 'right', } });
     } catch (error) {
-      enqueueSnackbar('Project not created!', { variant:'error', anchorOrigin:{vertical: 'bottom', horizontal: 'right',} });
+      enqueueSnackbar('Project not created!', { variant: 'error', anchorOrigin: { vertical: 'bottom', horizontal: 'right', } });
     }
   }
 
@@ -125,66 +128,94 @@ export function ProjectsProvider({ children }: ProjectsProviderProps) {
     setProjects(projects);
   }
 
-  function execShellCommand(cmd: string) {
-    const exec = require('child_process').exec;
+  function createFile(filePath: string, response: any) {
+    const buff = Buffer.from(response.data, "base64");
     return new Promise((resolve, reject) => {
-     exec(cmd, (error, stdout, stderr) => {
-      if (error) {
-       console.warn(error);
-      }
-      resolve(stdout || stderr);
-     });
+      fs.writeFile(filePath, buff, (err) => {
+        if (err) throw err;
+      })
     });
   }
 
-  async function runningDBDeploy(filePath: string, projectAlias: string){
+  function execShellCommand(cmd: string) {
+    const exec = require('child_process').exec;
+    return new Promise((resolve, reject) => {
+      exec(cmd, (error, stdout, stderr) => {
+        if (error) {
+          console.warn(error);
+        }
+        resolve(stdout || stderr);
+      });
+    });
+  }
+
+  async function runningDBDeploy(filePath: string, projectAlias: string) {
     console.log(`Running dbdeploy...`);
     await api.get(`dbdeployfile`)
-          .then(response => {
-            const buff = Buffer.from(response.data, "base64");
-            fs.writeFile(`${filePath}\\scripts\\DBDeploy.ps1`, buff, (err) => {
-              if (err) throw err;
-            });
-          })
-    const command = `C:\\Windows\\syswow64\\windowspowershell\\v1.0\\powershell.exe  -ExecutionPolicy Unrestricted -file "${filePath}\\scripts\\DBDeploy.ps1" -ScriptsDirectory "${filePath}\\scripts" -ServerInstance "EBKNTBOOK-0909\\SQLEXPRESS" -DestinationDBPrefix "${projectAlias}_" -DBUsername "sa" -DBPassword "pass123456."`; 
-    
+      .then(response => {
+        const buff = Buffer.from(response.data, "base64");
+        fs.writeFile(`${filePath}\\scripts\\DBDeploy.ps1`, buff, (err) => {
+          if (err) throw err;
+        });
+      })
+    const command = `C:\\Windows\\syswow64\\windowspowershell\\v1.0\\powershell.exe  -ExecutionPolicy Unrestricted -file "${filePath}\\scripts\\DBDeploy.ps1" -ScriptsDirectory "${filePath}\\scripts" -ServerInstance "EBKNTBOOK-0909\\SQLEXPRESS" -DestinationDBPrefix "${projectAlias}_" -DBUsername "sa" -DBPassword "pass123456."`;
+
     await execShellCommand(command);
   }
 
-  async function installProjectFiles(filePath: string, projectInput: Project){
-    let packages: Package [] = []
+  async function installProjectFiles(filePath: string, projectInput: Project) {
+    let packages: Package[] = []
     await api.get('package')
-    .then(response => {packages = response.data});
-    
-    if((os.platform() === 'darwin')){
-      packages = packages.filter(p=>p.isMacOS)
-    }else{
-      packages = packages.filter(p=>!p.isMacOS)
+      .then(response => { packages = response.data });
+
+    if ((os.platform() === 'darwin')) {
+      packages = packages.filter(p => p.isMacOS)
+    } else {
+      packages = packages.filter(p => !p.isMacOS)
+    }
+
+    for (const pack of packages) {
+
+      console.log(`Downloading package ${pack.name}...`);
+      await api.get(`package/${pack.name}/file`)
+        .then(async (response) => {
+          const buff = Buffer.from(response.data, "base64");
+          await fsPromises.writeFile(path.join(filePath, `${pack.name}.zip`), buff, (err) => {
+            if (err) { console.log(err); throw err };
+          })
+          // await createFile(path.join(filePath, `${pack.name}.zip`), response);
+        })
     }
 
     for (const pack of packages) {
       // Get and Install packages
-      console.log(`Downloading package ${pack.name}...`);
-      await api.get(`package/${pack.name}/file`)
-      .then(response => {
-        const buff = Buffer.from(response.data, "base64");
-        fs.writeFile(`${filePath}\\${pack.name}.zip`, buff, (err) => {
-          if (err) throw err;
-        });
-      })
+
+
       console.log(`Extracting package ${pack.name}...`);
-      await extract(`${filePath}\\${pack.name}.zip`, { dir: filePath })
+      if (os.platform() !== 'darwin') {
+        const command = `unzip ${path.join(filePath, `${pack.name}.zip`)}`;
+        try {
+          await execShellCommand(command);
+        } catch (error) {
+          console.log(error)
+          throw error;
+        }
+      }
+      else {
+        await extract(path.join(filePath, `${pack.name}.zip`), { dir: filePath })
+      }
+
       console.log(`Deleting file ${pack.name}...`);
-      fs.unlinkSync(`${filePath}\\${pack.name}.zip`);
+      await fsPromises.unlink(path.join(filePath, `${pack.name}.zip`));
 
       // Get and Install Configuration files
-      let packageFiles: PackageFile [] = []
+      let packageFiles: PackageFile[] = []
       await api.get('packageFile', {
         headers: {
           'packageID': pack.id
         }
       })
-      .then(response => {console.log(pack.id);console.log(response.data);packageFiles = response.data});
+        .then(response => { console.log(pack.id); console.log(response.data); packageFiles = response.data });
       for (const packFile of packageFiles) {
         console.log(`Downloading package File ${packFile.name}...`);
         await api.get(`packagefile/${packFile.fileType.name}/packagetemplate/${pack.templateName}/file`, {
@@ -194,30 +225,30 @@ export function ProjectsProvider({ children }: ProjectsProviderProps) {
             'projectID': projectInput.id
           }
         })
-        .then(response => {
-          const buff = Buffer.from(response.data, "base64");
-          fs.writeFile(`${filePath}\\${packFile.path}\\${packFile.name}.${packFile.fileType.extension}`, buff, (err) => {
-            if (err) throw err;
-          });
-        })
+          .then(response => {
+            const buff = Buffer.from(response.data, "base64");
+            fs.writeFile(`${filePath}\\${packFile.path}\\${packFile.name}.${packFile.fileType.extension}`, buff, (err) => {
+              if (err) throw err;
+            });
+          })
       }
     }
   }
 
-  async function installDatabases(filePath: string, projectInput: Project){
-    let databases: Database [] = []
+  async function installDatabases(filePath: string, projectInput: Project) {
+    let databases: Database[] = []
     await api.get('database')
-    .then(response => {databases = response.data});
+      .then(response => { databases = response.data });
 
     for (const data of databases) {
       console.log(`Downloading database ${data.name}...`);
       await api.get(`database/${data.name}/file`)
-      .then(response => {
-        const buff = Buffer.from(response.data, "base64");
-        fs.writeFile(`${filePath}\\${data.name}.zip`, buff, (err) => {
-          if (err) throw err;
-        });
-      })
+        .then(response => {
+          const buff = Buffer.from(response.data, "base64");
+          fs.writeFile(`${filePath}\\${data.name}.zip`, buff, (err) => {
+            if (err) throw err;
+          });
+        })
       console.log(`Extracting database ${data.name}...`);
       await extract(`${filePath}\\${data.name}.zip`, { dir: `${filePath}\\Scripts` })
       console.log(`Deleting file ${data.name}...`);
@@ -228,10 +259,10 @@ export function ProjectsProvider({ children }: ProjectsProviderProps) {
     console.log('dbdeploy runned!');
   }
 
-  async function installSites(filePath: string, projectInput: Project){
-    let sites: Site [] = []
+  async function installSites(filePath: string, projectInput: Project) {
+    let sites: Site[] = []
     await api.get('iissite')
-    .then(response => {sites = response.data});
+      .then(response => { sites = response.data });
 
     for (const data of sites) {
       console.log(`Downloading sites ${data.name}...`);
@@ -241,29 +272,29 @@ export function ProjectsProvider({ children }: ProjectsProviderProps) {
           'projectAlias': projectInput.alias,
         }
       })
-      .then(async(response) => {
-        const buff = Buffer.from(response.data, "base64");
-        fs.writeFile(`${filePath}\\${data.name}.ps1`, buff, (err) => {
-          if (err) throw err;
-        });
+        .then(async (response) => {
+          const buff = Buffer.from(response.data, "base64");
+          fs.writeFile(`${filePath}\\${data.name}.ps1`, buff, (err) => {
+            if (err) throw err;
+          });
 
-        const command = `C:\\Windows\\system32\\windowspowershell\\v1.0\\powershell.exe  -ExecutionPolicy Unrestricted -file "${filePath}\\${data.name}.ps1"`; 
-        await execShellCommand(command);
-        
-        fs.unlinkSync(`${filePath}\\${data.name}.ps1`);
-      })
+          const command = `C:\\Windows\\system32\\windowspowershell\\v1.0\\powershell.exe  -ExecutionPolicy Unrestricted -file "${filePath}\\${data.name}.ps1"`;
+          await execShellCommand(command);
+
+          fs.unlinkSync(`${filePath}\\${data.name}.ps1`);
+        })
     };
   }
 
-  async function installProject(projectInput: Project){
-    
+  async function installProject(projectInput: Project) {
+
     const { dialog } = require('electron').remote
 
     await dialog.showOpenDialog({ properties: ['openDirectory'] })
       .then(async response => {
         try {
           const projectPath = response.filePaths[0];
-          const filePath = `${projectPath}\\eBankit.${projectInput.alias}\\Main\\Source`
+          const filePath = path.join(projectPath, `eBankit.${projectInput.alias}`, 'Main', 'Source')
           if (!fs.existsSync(filePath)) {
             fs.mkdirSync(filePath, {
               recursive: true
@@ -285,20 +316,20 @@ export function ProjectsProvider({ children }: ProjectsProviderProps) {
           projecttoupdate.isInstalledWindows = (os.platform() !== 'darwin');
           projecttoupdate.isInstalledMac = (os.platform() === 'darwin');
 
-          await api.put(`project/${projectInput.id}`, {...projecttoupdate}).then(response => {
-              api.get('project')
-                .then(response => {console.log(response.data);setProjects(response.data)})
+          await api.put(`project/${projectInput.id}`, { ...projecttoupdate }).then(response => {
+            api.get('project')
+              .then(response => { console.log(response.data); setProjects(response.data) })
           });
-          enqueueSnackbar('Project intalled successfully!', { variant:'success', anchorOrigin:{vertical: 'bottom', horizontal: 'right',} });
+          enqueueSnackbar('Project intalled successfully!', { variant: 'success', anchorOrigin: { vertical: 'bottom', horizontal: 'right', } });
         } catch (error) {
-          enqueueSnackbar('Project not installed!', { variant:'error', anchorOrigin:{vertical: 'bottom', horizontal: 'right',} });
+          enqueueSnackbar('Project not installed!', { variant: 'error', anchorOrigin: { vertical: 'bottom', horizontal: 'right', } });
         }
-        
-      }) 
+
+      })
   }
 
   return (
-    <ProjectsContext.Provider value={{ projects, createProject, deleteProject, installProject}}>
+    <ProjectsContext.Provider value={{ projects, createProject, deleteProject, installProject }}>
       {children}
     </ProjectsContext.Provider>
   )
